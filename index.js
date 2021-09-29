5afa2709efa009d11717915ae690a5323c1f17c7
aldoortiz128 <aldoortiz128@gmail.com>
2021-09-29 01:18:56 +0000

    Finished all questions and the bonus question

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 //1
-function countToFifty(){
-  
+function countToFifty() {
+  for (let i = 1; i < 51; i++){
+      console.log ([i]);
+  }
 }
 
 //2
-function countFromOne(){
+function countFromOne(x){
+ for (i = 1; i <= x; i++) {
+ if (Number.isInteger(x)) {
+     console.log ([i]);
+}
+ }
   
 }
 
 //3
-function threeAndFiveInFifty(){
-  
+function threeOrFiveInFifty(){
+    for (let i=1; i < 50; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            console.log(i);
+        }
+    }
 }
+
 
 //4
-function threeAndFive(){
-  
+function threeOrFive(num){
+    for (let i=1; i < num; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            console.log(i);
+        }
+    }
+}
+
+//5
+function triple(numarray){
+    let arr = [];
+    for (let i = 0; i < numarray.length; i++){
+        arr.push(numarray[i]*3)
+        ; console.log(arr);}
 }
 
 
+countToFifty();
+countFromOne(5);
+threeOrFiveInFifty(10);
+threeOrFive(5);
+triple([3,5,6,4]);
